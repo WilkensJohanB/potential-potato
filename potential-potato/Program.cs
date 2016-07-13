6f0bc02f1f467f85ed913c0c6febb4d50fc52677
Johan Bjurstam <jbjurstam@wilkens.be>
2016-07-13 10:52:24 +0200

    !

---
+++ potential-potato/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potential_potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I say potato you say potato");
+
+            Console.ReadLine();
+        }
+    }
+}
